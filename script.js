ab206df2e70c00c80087eb66ea256a1b5c2995e2
Gemma <lanka13@gmail.com>
2020-04-17 17:06:45 +0200

    feat: new info on hologram project

--- script.js
+++ script.js
@@ -4,20 +4,18 @@ function apearProject(id) {
     
     for (i = 0; i < descendents.length; ++i) {
         e = descendents[i];
-        if (e.style.display === "block") {
+        if (e.style.display === "flex") {
             e.style.display = "none";
           }
     }
-    document.getElementById('infoProject'+id).style.display = 'block';
-  /*  
-    var icons = document.getElementById('icons');
-    icon = icons.getElementsByTagName('*');
-    for (j = 0; j < descendents.length; ++j) {
-        p = icon[j];
-        p.style.width = "100%";
-        p.style.height = "100%";
-    }
-    document.getElementById('icon'+id).style.width = "110%";
-    document.getElementById('icon'+id).style.height = "110%";
-*/
+    document.getElementById('infoProject'+id).style.display = 'flex';
+  }
+
+  function changepage(id) {
+    document.getElementById('projectText'+ id).style.display = "none";
+    document.getElementById('projectText'+ (++id)).style.display = 'block';
+  }
+  function lastpage(id){
+    document.getElementById('projectText'+ id).style.display = "none";
+    document.getElementById('projectText'+ ('1')).style.display = 'block';
   }
